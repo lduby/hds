f827f25fb983b249bcf396bac6554466c280a3c8
Laetitia Duby <laetitia.duby@gmail.com>
2012-08-24 11:59:45 +0200

    Displaying reviews count, ratings count and average rating for a user in his profile

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -7,4 +7,46 @@ class Profile < ActiveRecord::Base
   def name
     "#{first_name} #{last_name}"
   end
+
+  def average_rating  	
+  	@shelves_id = Array.new()
+  	@conditions = "shelf_id IN ("
+  	shelves.each do |s| 
+  	  if s == shelves.first
+  		@conditions << s.id.to_s
+  	  else
+  	  	@conditions << ", "+s.id.to_s
+  	  end
+  	end
+  	@conditions << ") AND details IS NOT NULL"
+    Review.average(:rating, :conditions => @conditions)
+  end
+
+  def ratings_count
+  	@shelves_id = Array.new()
+  	@conditions = "shelf_id IN ("
+  	shelves.each do |s| 
+  	  if s == shelves.first
+  		@conditions << s.id.to_s
+  	  else
+  	  	@conditions << ", "+s.id.to_s
+  	  end
+  	end
+  	@conditions << ") AND details IS NOT NULL"
+    Review.count(:rating, :conditions => @conditions)
+  end
+
+  def reviews_count
+  	@shelves_id = Array.new()
+  	@conditions = "shelf_id IN ("
+  	shelves.each do |s| 
+  	  if s == shelves.first
+  		@conditions << s.id.to_s
+  	  else
+  	  	@conditions << ", "+s.id.to_s
+  	  end
+  	end
+  	@conditions << ") AND details IS NOT NULL"
+    Review.count(:id, :conditions => @conditions)
+  end
 end
