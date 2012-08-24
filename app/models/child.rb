170d4488c50a3eac9bd3c233c659f2f73ed57537
Laetitia Duby <laetitia.duby@gmail.com>
2012-08-24 11:27:29 +0200

    Calculating children age and displaying it in the children profiles

--- app/models/child.rb
+++ app/models/child.rb
@@ -2,4 +2,9 @@ class Child < ActiveRecord::Base
   belongs_to :profile 
   has_many :books, :through => :reviews
   attr_accessible :birth_date, :first_name
+
+  def age
+  	now = Time.now.utc.to_date
+  	now.year - birth_date.year - ((now.month > birth_date.month || (now.month == birth_date.month && now.day >= birth_date.day)) ? 0 : 1)
+  end
 end
