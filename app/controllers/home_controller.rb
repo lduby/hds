1122143ed4ed0da512a13ccd877ba513df326539
Laetitia DUBY <laetitia.duby@gmail.com>
2013-06-24 23:32:20 +0200

    Second draft of the home page using Bookblock slider

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,19 @@ class HomeController < ApplicationController
   def index
     @books = Book.all
     @last_entries = Book.order("created_at DESC").limit(10)
+    @most_reviewed = Book.all(:select => "books.*, COUNT(book_id) as reviews_count",
+            :joins  => "LEFT JOIN reviews AS reviews ON reviews.book_id = books.id",
+            :group  => "reviews.book_id",
+            :order  => "reviews_count DESC",
+            :conditions  => "reviews.details IS NOT NULL",
+            :limit => 5)
+    @populars = Book.all(:select => "books.*, AVG(rating) as average_rating",
+            :joins  => "LEFT JOIN reviews AS reviews ON reviews.book_id = books.id",
+            :group  => "reviews.book_id",
+            :order  => "average_rating DESC",
+            :conditions  => "reviews.rating IS NOT NULL",
+            :limit => 5)
+    #@coup_coeur = Book.order("created_at DESC").limit(10)
     if user_signed_in?
       if current_user.profile.nil?
         logger.debug(current_user.id)
